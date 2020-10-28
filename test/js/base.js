32e3a8955b3a92f0bbd63aa2cfaa3159c4b53395
kireetipydisetty <pkireeti009@gmail.com>
2020-10-28 18:40:21 +0530

    first

---
+++ test/js/base.js
@@ -0,0 +1,10 @@
+function fun_1(){
+    x = document.querySelector(".home")
+    x1 = document.querySelector(".login")
+    x2 = document.querySelector(".logout")
+    x3 = document.querySelector(".register")
+    x.style.color="white"
+    x1.style.color="white"
+    x2.style.color="white"
+    x3.style.color="white"
+}
